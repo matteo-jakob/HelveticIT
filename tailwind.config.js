a0b712d221292d3e52908c883319b1063cfc0cd8
Matteo <taro@yamada.com>
2025-02-21 13:14:14 +0100

    improvements

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,27 @@
 module.exports = {
   content: ["./*.html"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        // NOT GETTING USED RN TODO IMPLEMENT IN CODE
+        primary: {
+          DEFAULT: "#1E40AF",
+          dark: "#1E3A8A",
+        },
+        secondary: {
+          DEFAULT: "#DB2777",
+          dark: "#BE185D",
+        },
+        accent: {
+          DEFAULT: "#F59E0B",
+          dark: "#D97706",
+        },
+        gray: {
+          DEFAULT: "#6B7280",
+          dark: "#4B5563",
+        },
+      },
+    },
   },
   plugins: [],
 };
